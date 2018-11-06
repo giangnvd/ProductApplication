d749d218055cf63f1e20468d54b224abd99567ce
giangnvd <giangnvd00570@fpt.edu.vn>
2018-11-06 21:03:36 +0700

    Application

---
+++ ProductApplication/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using ProductApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductApplication.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}   
